f072b09fa923e278de50fafb6a9472146b063e0f
Joshua Newhouse <joshua.b.newhouse@gmail.com>
2021-02-15 13:47:55 -0600

    Creates ubuntu setup.sh script

---
+++ ubuntu-setup/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -xe
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 8AA7AF1F1091A5FD
+sudo add-apt-repository 'deb [arch=amd64] http://repo.sawtooth.me/ubuntu/chime/stable bionic universe'
+sudo apt-get update && sudo apt-get install -y sawtooth python3-sawtooth-poet-cli python3-sawtooth-poet-engine python3-sawtooth-poet-families
+
+dpkg -l '*sawtooth*'
+
+sawtooth keygen my_key
+
+sudo sawadm keygen
