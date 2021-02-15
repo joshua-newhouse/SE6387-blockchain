ca8e07be4b1bba0d5d0863cfad79423b1b3622a3
Joshua Newhouse <joshua.b.newhouse@gmail.com>
2021-02-14 21:10:47 -0600

    Adjusting the docker compose environment

--- start-sawtooth.sh
+++ start-sawtooth.sh
@@ -2,21 +2,25 @@
 
 source util/logging.sh
 
-function StartNode() {
+source conf/peers.env
+
+function VerifyRestAPIUp() {
     local nodeID=${1}
 
-    for envFile in ./conf/*.env; do
-        docker-compose --env-file "${envFile}" -f sawtooth-default-poet.yaml up -d
-        [[ $? -ne 0 ]] && $WarnMessage "Failed bringing up node configured in ${envFile}"
-    done
+    local response="$(docker exec -t ${REST_API_CTX_PFX}-${nodeID} sh -c \"ps --pid 1 fw\")"
+    [[ $? -ne 0 ]] && $WarnMessage "REST API ${nodeID} is not up: ${response}" && return 1
+
+    return 0
 }
 
 function Main() {
     docker-compose -f sawtooth-default-poet.yaml up -d
     [[ $? -ne 0 ]] && $ErrMessage "Error starting Sawtooth network" && exit 1
 
-    docker exec -t sawtooth-rest-api-default-0 sh -c "ps --pid 1 fw"
-    [[ $? -ne 0 ]] && $ErrMessage "Error verifying REST API" && exit 1
+    for nodeID in {0..4}; do
+        VerifyRestAPIUp ${nodeID}
+        [[ $? -ne 0 ]] && $ErrMessage "Error verifying REST API" && exit 1
+    done
 
     docker exec -t sawtooth-shell-default \
         sh -c "curl http://sawtooth-rest-api-default-0:8008/peers"
